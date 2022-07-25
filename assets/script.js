941f610b56028a4ba60c7b8c7d585c866b3e3243
twills <treav.wills@gmail.com>
2022-07-25 12:40:19 -0700

    use moment to compare each time row to the current time and change color accordingly.

--- assets/script.js
+++ assets/script.js
@@ -9,8 +9,6 @@ $('#text-for-btn03').val(localStorage.getItem('btn03Text'));
 $('#text-for-btn04').val(localStorage.getItem('btn04Text'));
 $('#text-for-btn05').val(localStorage.getItem('btn05Text'));
 
-
-
 $('.saveBtn').on('click', function(event){
     // for testing/setting up
     // console.log(idOfTextToSave);
@@ -20,4 +18,43 @@ $('.saveBtn').on('click', function(event){
     var idOfTextToSave = '#text-for-'+this.id;
     var localStorageVariable = this.id + 'Text';
     localStorage.setItem(localStorageVariable, $(idOfTextToSave).val());
-})
+});
+
+console.log("children: " + JSON.stringify($('#whole-workday').children().attr('id')));
+
+console.log(parseInt($('#whole-workday').children().attr('id')));
+
+console.log($('whole-workday'));
+
+function timeBlockState() {
+    var now = moment().hours();
+    $('.row').each(function() {
+        var hour = parseInt($(this).attr('id').slice(-2));
+        console.log(hour);
+        var timeBlock = parseInt($(this).attr('id').split('-'));
+        if(hour < now){
+            $(this).addClass('past');
+            $(this).removeClass('present');
+            $(this).removeClass('future');
+        }
+        else if (hour === now) {
+            $(this).addClass('present');
+            $(this).removeClass('past');
+            $(this).removeClass('future');
+        }
+        else if (hour > now){
+            $(this).removeClass('present');
+            $(this).removeClass('past');
+            $(this).addClass('future');
+        }
+    });
+}
+
+timeBlockState();
+var interval = setInterval(timeBlockState, 15000);
+
+function currentDay(){
+$('#currentDay').text(moment().format('MMMM Do YYYY, h:mm a'));
+};
+
+var currentDayInterval = setInterval(currentDay, 1000);
